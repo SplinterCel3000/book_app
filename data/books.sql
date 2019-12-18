e4234c8b2f113323cfee40e076d8bf65372e25bb
‘peter-robert-cole’ <petercole3@gmail.com>
2019-12-18 10:39:52 -0800

    part 1 of feature 1

--- data/books.sql
+++ data/books.sql
@@ -5,10 +5,10 @@ CREATE TABLE books (
 id SERIAL PRIMARY KEY,
 author VARCHAR(255),
 title VARCHAR(255),
-isbn NUMERIC,
+isbn VARCHAR(20),
 image_url TEXT,
 description TEXT,
-bookshelf TEXT
+bookshelf VARCHAR(255)
 );
 
 INSERT INTO books (author, title, isbn, image_url, description, bookshelf) VALUES ('Into Thin Air', 'Jon Krakauer', 9780307475251, 'http://books.google.com/books/content?id=iTf5X22gChAC&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE72m8PbtHVg4mzah1ZKcPwFR1m5rcU4rE3XP188rgEfWhs57hq5Rko4qfUlqkib9kQp2Q4CwmE0J2iDIjRDsMhvj2PoH_3D7WiuH5rZrWqOpLqCvrhB82ITZksZKlrQQm86SVOH-&source=gbs_api', 'test1 test2 test3', 'default');
